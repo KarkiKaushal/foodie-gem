06117acdeb772a864b9349be5ec2e9c394dfc146
Kaushal Karki <kaushal.karki@maropost.com>
2022-04-08 11:37:09 +0530

    First Commit

---
+++ lib/foodie/food.rb
@@ -0,0 +1,18 @@
+require 'active_support/inflector'
+module Foodie
+    class Food
+      def self.portray(food)
+        if food.downcase == "broccoli"
+          "Gross!"
+        elsif food.downcase == "banana"
+          "Yummy!"  
+        else
+          "Delicious!"
+        end
+      end
+
+      def self.pluralize(word)
+        word.pluralize
+      end
+    end
+  end
